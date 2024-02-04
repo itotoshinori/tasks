65fcad0f1539aeb8052585367f9e1990680ba7b0
T-I <tito40358@gmail.com>
2024-02-04 23:43:23 +0900

    トップ画面微調整

--- resources/ts/pages/tasks/componets/TaskItemBox.tsx
+++ resources/ts/pages/tasks/componets/TaskItemBox.tsx
@@ -245,10 +245,10 @@ const TaskItemBox: React.VFC<Props> = ({ task, compliteCss, handleSearchWord }) 
     }
 
     const borderLeft = () => {
-        if (String(task.term) == getToday()) {
-            return "blue"
-        } else if (task.title.includes("🔥")) {
+        if (task.title.includes("🔥")) {
             return "red"
+        } else if (String(task.term) == getToday()) {
+            return "blue"
         }
     }
     return (
